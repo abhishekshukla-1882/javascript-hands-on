af91f76f976e66467d44f5b5a059a2afe8248904
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 13:35:40 +0530

    local Scope and Functions

--- src/index.js
+++ src/index.js
@@ -196,3 +196,14 @@ function fun2() {
   }
   console.log(output);
 }
+function myLocalScope() {
+    var myVar
+    // Only change code below this line
+  
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
