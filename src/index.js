65d10b538c56cf64305ef5866f84b6c1141be67d
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 14:28:58 +0530

    Chaining If Else Statements

--- src/index.js
+++ src/index.js
@@ -481,4 +481,22 @@ function testStrictNotEqual(val) {
             return "Greater than or equal to 10";
         }
     }
-    orderMyLogic(7);  
+    orderMyLogic(7);
+    function testSize(num) {
+        // Only change code below this line
+      
+        if (num < 5) return "Tiny";
+      
+        else if (num < 10) return "Small";
+      
+        else if (num < 15) return "Medium";
+      
+        else if (num < 20) return "Large";
+      
+        else return "Huge";
+      
+        // Only change code above this line
+      }
+      
+      // Change this value to test
+      testSize(7);  
