2ac71f3eb15a39e8922e8a41e27f0efe6825fef2
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 15:35:18 +0530

    Accessing Nested Arrays

--- src/index.js
+++ src/index.js
@@ -848,4 +848,27 @@ var myStorage = {
   
   // Only change code below this line
   
-  var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line  
+  var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+  // Setup
+var myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  // Only change code below this line
+  
+  var secondTree = myPlants[1].list[1]; // Change this line  
