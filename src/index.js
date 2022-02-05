a09c64779acebb56c787bc52285d6c41d9568255
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 15:31:31 +0530

    Manipulating Complex Objects

--- src/index.js
+++ src/index.js
@@ -810,4 +810,26 @@ function phoneticLookup(val) {
    
   
     // Only change code above this line
-  }  
+  }
+  const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+   {
+      "artist": "Bob",
+      "title": "Hello",
+      "release_year": 1950,
+      "formats": [
+        "CS",
+        "8T"
+      ]
+    }
+  ];  
