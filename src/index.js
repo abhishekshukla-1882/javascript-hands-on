9881c3abcd6246fc5a8eb09c5e9b14ae04189cde
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 14:04:10 +0530

    Comparison with the Less Than Operator

--- src/index.js
+++ src/index.js
@@ -381,3 +381,20 @@ function testStrictNotEqual(val) {
   
   // Change this value to test
   testGreaterOrEqual(10);
+  function testLessThan(val) {
+    if (val < 24) {  // Change this line
+      return "Under 25";
+    }
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  // Change this value to test
+  testLessThan(10);
