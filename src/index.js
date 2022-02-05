e1c9d0f0f95df127ff711f5d2ff6816180a14b29
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 15:33:21 +0530

    Accessing Nested Objects

--- src/index.js
+++ src/index.js
@@ -832,4 +832,20 @@ function phoneticLookup(val) {
         "8T"
       ]
     }
-  ];  
+  ];
+  // Setup
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  // Only change code below this line
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line  
