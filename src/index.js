94b3d63ea36ae6fde5b23d1c199057e47e2deeee
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 10:55:56 +0530

    Understanding Uninitialized Variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-var myName;
-var a;
-a = 7;
-var b;
-b = a
-var a  =9;
+// var myName;
+// var a;
+// a = 7;
+// var b;
+// b = a
+// var a  =9;
 var myFirstName = "Abhishek"
 var myLastName = "Shukla"
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
