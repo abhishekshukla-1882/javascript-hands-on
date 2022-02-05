016cf4a7a5674d1e2a5a634f760f0db34ee70fcc
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 13:11:26 +0530

    Manipulate Arrays With pop()

--- src/index.js
+++ src/index.js
@@ -148,3 +148,8 @@ const myArray = [
   ];
   
   const myData = myArray[2][1];
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop()
+// Only change code below this line
