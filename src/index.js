aab802cfc6bbdf1eb3d62f28613a9f6cb46bd77a
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-05 15:43:31 +0530

    Record Collection

--- src/index.js
+++ src/index.js
@@ -872,3 +872,42 @@ var myPlants = [
   // Only change code below this line
   
   var secondTree = myPlants[1].list[1]; // Change this line  
+// Setup
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  // var collectionCopy = JSON.parse(JSON.stringify(collection));
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    if (value === '') {
+      delete records[id][prop]; // If the value is empty remove the prop
+    } else if (prop !== "tracks") {
+      records[id][prop] = value;
+    } else {
+      if (!records[id].hasOwnProperty('tracks')) {
+        records[id].tracks = [];
+        records[id].tracks.push(value);
+      } else {
+        records[id].tracks.push(value);
+      }
+    }
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
